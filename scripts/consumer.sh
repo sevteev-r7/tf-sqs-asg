25ae7adb377b592b2ae624e9f83246b408b95208
sevteev-r7 <sergey_evteev@rapid7.com>
2024-11-13 13:21:27 +0100

    minor modifications

--- scripts/consumer.sh
+++ scripts/consumer.sh
@@ -4,7 +4,7 @@
 [ "$1" != "" ] && msgs_n=$1 || { echo "Missing number of messages to consume"; exit 1; }
 
 #set the env vars before use
-[ "$AWS_DEFAULT_REGION" != "" ] && region=$AWS_DEFAULT_REGION || { echo "Missing AWS_DEFAULT_REGION env var"; exit 1; }
+[ -z "$AWS_DEFAULT_REGION" ] && { echo "Missing AWS_DEFAULT_REGION env var"; exit 1; }
 [ "$SQS_QUEUE_URL" != "" ] && queue_url=$SQS_QUEUE_URL || { echo "Missing SQS_QUEUE_URL env var"; exit 1; }
 
 export AWS_PAGER=""
@@ -13,7 +13,7 @@ batch_file=$(uuidgen)
 
 while [ $msgs_n -ne 0 ]; do
   [ $msgs_n -lt 10 ] && batch_size=$msgs_n || batch_size=10
-  IFS=$'\n' read -r -d '' -a batch < <( aws --region $region sqs receive-message --queue-url $queue_url --max-number-of-messages $batch_size | jq -r '.Messages[].ReceiptHandle' && printf '\0' )
+  IFS=$'\n' read -r -d '' -a batch < <( aws sqs receive-message --queue-url $queue_url --max-number-of-messages $batch_size | jq -r '.Messages[].ReceiptHandle' && printf '\0' )
   echo -n "Consuming batch of ${#batch[@]}: "
   echo "[" > $batch_file
 
@@ -26,7 +26,10 @@ while [ $msgs_n -ne 0 ]; do
 EOT
   done
   echo "]" >> $batch_file
-  #msgs_n=$(echo "$msgs_n - ${#batch[@]}" | bc)  
-  aws --region $region sqs delete-message-batch --queue-url $queue_url --entries file://$batch_file > /dev/null 2>&1 \
-      && { rm -f $batch_file; msgs_n=$(echo "$msgs_n - ${#batch[@]}" | bc); echo "Done"; } || { echo "Failed"; exit 1; }
+  aws sqs delete-message-batch --queue-url $queue_url --entries file://$batch_file > /dev/null 2>&1
+  ret_code=$?
+  rm -f $batch_file
+  [ $ret_code -ne 0 ] &&  { echo "Failed"; exit 1; }
+  msgs_n=$(echo "$msgs_n - ${#batch[@]}" | bc)
+  echo "Done";
 done
