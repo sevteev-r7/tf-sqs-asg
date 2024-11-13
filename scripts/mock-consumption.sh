886e157e4aeddc672e36c560e2a8de93f5db9fac
sevteev-r7 <sergey_evteev@rapid7.com>
2024-11-13 13:08:08 +0100

    resolved conflicts

--- scripts/mock-consumption.sh
+++ scripts/mock-consumption.sh
@@ -3,6 +3,10 @@
 export SQS_QUEUE_URL=https://sqs.eu-west-1.amazonaws.com/<AWS account ID>/tf_sqs_poc
 export AWS_DEFAULT_REGION=eu-west-1
 
+#metric target
+worker_backlog=10
+
+#delay
 long=20
 short=3
 
@@ -22,7 +26,7 @@ esac
 
 while true; do
     fleet_size=$(aws ec2 describe-instances --query 'Reservations[].Instances[].InstanceId' --filters Name=instance-state-name,Values=running | jq -r '.[]' | wc -l)
-    backlog=$(echo "$fleet_size * 10" | bc)
+    backlog=$(echo "$fleet_size * $worker_backlog" | bc)
     delay=$((1 + $RANDOM % $processing_time))
     batch=$((1 + $RANDOM % $backlog))
     echo "$fleet_size workers"
